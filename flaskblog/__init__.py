9a6d32f6b548e95ae08936eaffdbd62cb4ce95b8
FemiBlack <tadefemi2@hotmail.com>
2021-02-19 12:19:29 +0100

    first commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '***REMOVED***'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = '***REMOVED***'
+# app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = '***REMOVED***'
+# app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
+mail = Mail(app)
+
+from flaskblog.users.routes import users
+from flaskblog.main.routes import main
+from flaskblog.posts.routes import posts
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
