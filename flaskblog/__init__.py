59d9f6b81b18816259e4a1b8df1a794a6e19fa3d
femi_black <tadefemi02@gmail.com>
2021-02-21 19:01:54 +0100

    Changes made to .gitignore dotenv requirement added environmental variables added

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -4,10 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '***REMOVED***'
-app.config['SQLALCHEMY_DATABASE_URI'] = '***REMOVED***'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -16,9 +18,9 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = '***REMOVED***'
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 # app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = '***REMOVED***'
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 # app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
 mail = Mail(app)
 
